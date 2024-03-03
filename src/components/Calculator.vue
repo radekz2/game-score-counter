fdaff8f35284d0192121fb46eb7181b9d761e683
Radek Zajkowski <radek@wellesleystreet.com>
2024-03-03 02:50:58 +0000

    add Calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,10 +1,22 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 
+const count = ref(0)
 </script>
 
 <template>
   <div>
-    <h1>Calculator</h1>
+    <h1>Player</h1>
+    <div>
+      <input name="playerName" placeholder="Player Name" value=""/> 
+    </div>
+    <div>
+      Score <span id="score">{{ count }}</span>
+    </div>
+    <div>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
   </div>
 </template>
 
